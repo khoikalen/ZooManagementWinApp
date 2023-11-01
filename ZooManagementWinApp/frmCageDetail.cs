7b4869be4d9b99ca30807292cdf35ab09db784dd
kanatsumi2003 <naduccva@gmail.com>
2023-11-01 23:18:31 +0700

    Change form for moving cage detail

--- ZooManagementWinApp/frmCageDetail.cs
+++ ZooManagementWinApp/frmCageDetail.cs
@@ -98,12 +98,12 @@ namespace ZooManagementWinApp
         private void btnSave_Click(object sender, EventArgs e)
         {
             var cage = new Cage();
-            var selectedCage = GetCageObject();
             CageRepository = new CageRepository();
             try
             {
                 if (IsMoveCage)
                 {
+                    var selectedCage = GetCageObject();
                     if (selectedCage.Quantity == 0)
                     {
                         cage = new Cage
@@ -119,9 +119,12 @@ namespace ZooManagementWinApp
                         UpdateAnimalCage(cage.Id);
                         cage.Quantity = animalRepository.AnimalQuantityInCage(cage.Id);
                         CageRepository.UpdateCage(cage);
-                    } else if(selectedCage.Quantity != 0) {
+                    }
+                    else if (selectedCage.Quantity != 0)
+                    {
                         var animal = animalRepository.GetAnimalByCageID(selectedCage.Id);
-                        if(animalInformation.Specie.Equals(animal.First().Specie)){
+                        if (animalInformation.Specie.Equals(animal.First().Specie))
+                        {
                             cage = new Cage
                             {
                                 Id = int.Parse(txtCageID.Text),
@@ -185,19 +188,21 @@ namespace ZooManagementWinApp
                     }
                     if (cage.CageStatus == "Empty")
                     {
-                        
+
                         if (cage.Quantity > 0)
                         {
                             MessageBox.Show("There are still animal in cage, can not change to empty cage");
-                        } else
+                        }
+                        else
                         {
                             CageRepository.UpdateCage(cage);
                         }
-                    }   else
+                    }
+                    else
                     {
                         CageRepository.UpdateCage(cage);
                     }
-                    
+
                 }
             }
             catch (Exception ex)
